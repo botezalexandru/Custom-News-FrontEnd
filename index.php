fa50129813c4bb05c2bf7ce64843703f867f2e7d
Alexandru Botez <yobapq@gmail.com>
2017-05-10 00:02:58 +0300

    changed project structure

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 error_reporting(E_ALL & ~E_NOTICE);
- require_once('views/header.php') ;
+// require_once('views/header.php') ;
   require_once('proiect.html') ;
 
 $action = '';
@@ -9,17 +9,17 @@ if (isset($_REQUEST['action'])) $action = $_REQUEST['action'];
 
 switch($action){
 	case "register":
-		
+
 		require_once('class/class.user.php');
 		register($_POST['username'], $_POST['password'], $_POST['email'] );
-		
+
 
 	break;
 	case "login":
 		require_once('class/class.user.php');
-		
+
 		login($_POST['username'], $_POST['password'] );
-		
+
 
 	break;
 	case "logout":
--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-$(function() {
-  //here goes your js
+$(function () {
+  $('.flipbook').turn({
+    // Width
+
+    width: 1000,
+
+    // Height
+
+    height: 600,
+
+    // Elevation
+
+    elevation: 50,
+
+    // Enable gradients
+
+    gradients: true,
+
+    // Auto center this flipbook
+
+    autoCenter: true
+
+  });
 });
