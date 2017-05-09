e2815da817441cba9d046aea056b642a592e6172
vasiliupaula <paulaandreeavasiliu@gmail.com>
2017-05-09 04:08:58 -0700

    Add files via upload

---
+++ views/header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<?php require_once('header.css') ?>
+
+<body>
+
+
+
+</body>
+</html>
---
+++ views/home.php
@@ -0,0 +1,9 @@
+<?php require_once('header.php') ?>
+<style>
+
+</style>
+<html>
+<div><font size="7" color= "grey"><center>Welcome! </font></center></div>
+
+</html>
+
