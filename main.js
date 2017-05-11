3206c551dd1ac6bf333e18e70ff731d03bcf2ede
AndreiCristianLupu <lupu.andrei96@yahoo.com>
2017-05-11 22:52:37 +0300

    Add files via upload
    am marit dimensiunile, era prea mica pagina.

---
+++ main.js
@@ -0,0 +1,24 @@
+$(function () {
+  $('.flipbook').turn({
+    // Width
+
+    width: 1050,
+
+    // Height
+
+    height: 700,
+
+    // Elevation
+
+    elevation: 50,
+
+    // Enable gradients
+
+    gradients: true,
+
+    // Auto center this flipbook
+
+    autoCenter: true
+
+  });
+});
